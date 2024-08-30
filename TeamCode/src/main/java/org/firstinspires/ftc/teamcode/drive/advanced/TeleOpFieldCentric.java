895d9d5371d8348f3674ed6053c7fcf84db4dbae
Yohan Sutjandra <yohan.sutjandra@tes-h2.com>
2024-08-30 16:16:29 -0500

    Add heading correction using PIDFController

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/advanced/TeleOpFieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/advanced/TeleOpFieldCentric.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.drive.advanced;
 
+import com.acmerobotics.roadrunner.control.PIDFController;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 /**
@@ -18,10 +20,21 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  */
 @TeleOp(group = "advanced")
 public class TeleOpFieldCentric extends LinearOpMode {
+
+    // Declare a PIDF Controller to regulate heading
+    // Use the same gains as SampleMecanumDrive's heading controller
+    private PIDFController headingController = new PIDFController(SampleMecanumDrive.HEADING_PID);
+
     @Override
     public void runOpMode() throws InterruptedException {
         // Initialize SampleMecanumDrive
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        double savedHeading = 0;
+
+        // Set input bounds for the heading controller
+        // Automatically handles overflow
+        headingController.setInputBounds(-Math.PI, Math.PI);
+
 
         // We want to turn off velocity control for teleop
         // Velocity control per wheel is not necessary outside of motion profiled auto
@@ -46,15 +59,40 @@ public class TeleOpFieldCentric extends LinearOpMode {
                     -gamepad1.left_stick_x
             ).rotated(-poseEstimate.getHeading());
 
+            // Create a vector from the gamepad x/y inputs which is the field relative movement
+            // Then, rotate that vector by the inverse of that heading for field centric control
+            Vector2d fieldFrameInput = new Vector2d(
+                    -gamepad1.left_stick_y,
+                    -gamepad1.left_stick_x
+            );
+            Vector2d robotFrameInput = fieldFrameInput.rotated(-poseEstimate.getHeading());
+
             // Pass in the rotated input + right stick value for rotation
             // Rotation is not part of the rotated input thus must be passed in separately
+
+            double rotation = (
+                    -gamepad1.right_stick_x
+            );
+
+            // set headingInput to rotation if gamepad shows activities
+            // otherwise rotate to correct heading
+            double headingInput = rotation;
+            if (Math.abs(rotation) <= 0.05) {
+                headingInput = (headingController.update(poseEstimate.getHeading())
+                        * DriveConstants.kV)
+                        * DriveConstants.TRACK_WIDTH;
+            }
+
             drive.setWeightedDrivePower(
                     new Pose2d(
-                            input.getX(),
-                            input.getY(),
-                            -gamepad1.right_stick_x
+                            robotFrameInput,
+                            headingInput
                     )
             );
+
+            if (Math.abs(rotation) > 0.05) {
+                headingController.setTargetPosition(poseEstimate.getHeading());
+            }
 
             // Update everything. Odometry. Etc.
             drive.update();
